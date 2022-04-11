f8ebf7370a5370021a3f78a66d4104e21d5af82a
just-osaze <osaigbevo.osaz@gmail.com>
2022-04-11 15:42:52 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: Always success (0)
+  */
+
+int main(void)
+{
+	char lowerCase = 'a';
+	char upperCase = 'A';
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowerCase);
+		lowerCase++;
+	}
+
+	while (upperCase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+
+	putchar("\n");
+
+	return (0);
+}

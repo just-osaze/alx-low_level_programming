ef409de4227f1401b6aa1d1f958d9ba629031f4e
just-osaze <osaigbevo.osaz@gmail.com>
2022-04-11 16:20:18 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always success (0)
+ */
+
+int main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+		printf("%i", i);
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}

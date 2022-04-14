d478412258971d27d040826f592518e99257df37
just-osaze <osaigbevo.osaz@gmail.com>
2022-04-13 19:33:40 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _isalpha - prints uppercase characters
+  * Return: returns uppercase characters
+  * @c: variable
+  *
+  */
+
+int _isalpha(in c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}

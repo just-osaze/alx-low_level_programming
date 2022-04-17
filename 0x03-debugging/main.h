7605803ffc7c0ea44168a8bb20c6e1ed43a2634a
just-osaze <osaigbevo.osaz@gmail.com>
2022-04-16 19:25:12 -0700

    multiple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+/**
+ * print the correct output when given a case of 0
+ */
+
+int largest_number(int, int, int);
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+void print_remaining_days(int month, int day, int year);
+
+
+int convert_day(int month, int day);
+
+#endif

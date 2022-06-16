6a72704266c028da663a08b2d56db2efb8cbb67a
just-osaze <osaigbevo.osaz@gmail.com>
2022-06-16 01:29:11 -0700

    0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,25 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_H
+#define main_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
-/**
- * print the correct output when given a case of 0
- */
-
-int largest_number(int, int, int);
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
-
-
 int convert_day(int month, int day);
 
-#endif
+#endif /* main_H */
